b71bcd9d27e07cbde85461dca2563528599fa08e
kberdos <kazuya_erdos@brown.edu>
2023-08-28 20:57:17 -0400

    create page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,12 @@ import { PageLayout } from "@/components/PageLayout";
 export default function Home() {
   return (
     <PageLayout>
-      <div className="flex align-middle"></div>
+      <div className="flex justify-center h-full">
+        <div className="border rounded-xl w-11/12 h-5/6 flex flex-col items-center justify-center mt-4">
+          <p className="font-bold text-6xl mb-4">Abbot Appraisals</p>
+          <p className="font-semibold text-2xl">Jason Erdos</p>
+        </div>
+      </div>
     </PageLayout>
   );
 }
